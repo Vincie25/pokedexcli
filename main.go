26f6b9316b33ffbd5bbd1d180f797242ee516411
Vincie25 <googlfreestyle@gmail.com>
2026-03-10 18:50:40 +0100

    conecting with pokeapi

--- main.go
+++ main.go
@@ -5,23 +5,41 @@ import (
     "fmt"
     "os"
     "strings"
+    "encoding/json"
+    "io"
+    "net/http"
 )
 
 type cliCommand struct {
     name        string
     description string
-    callback    func() error
+    callback    func(*config) error
+}
+
+type pokeResponse struct {
+	Count    int     `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+type config struct {
+    Next     *string
+    Previous *string
 }
 
 var commands map[string]cliCommand
 
-func commandExit() error {
+func commandExit(cfg *config) error {
     fmt.Println("Closing the Pokedex... Goodbye!")
     os.Exit(0)
     return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
     fmt.Println("Welcome to the Pokedex!")
     fmt.Println("Usage:\n")
     for _, cmd := range commands {
@@ -30,7 +48,74 @@ func commandHelp() error {
     return nil
 }
 
+func commandMap(cfg *config) error {
+	url := "https://pokeapi.co/api/v2/location-area"
+	if cfg.Next != nil {
+		url = *cfg.Next
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var pokeRes pokeResponse
+	err = json.Unmarshal(body, &pokeRes)
+	if err != nil {
+		return err
+	}
+
+	cfg.Next = pokeRes.Next
+	cfg.Previous = pokeRes.Previous
+
+	for _, location := range pokeRes.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.Previous == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	res, err := http.Get(*cfg.Previous)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var pokeRes pokeResponse
+	err = json.Unmarshal(body, &pokeRes)
+	if err != nil {
+		return err
+	}
+
+	cfg.Next = pokeRes.Next
+	cfg.Previous = pokeRes.Previous
+
+	for _, location := range pokeRes.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
 func main() {
+    cfg := &config{}
     commands = map[string]cliCommand{
         "exit": {
             name:        "exit",
@@ -41,6 +126,16 @@ func main() {
             name:        "help",
             description: "Displays a help message",
             callback:    commandHelp,
+        },
+        "map": {
+			name:        "map",
+			description: "Displays next 20 location areas",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays previous 20 location areas",
+            callback:    commandMapb,
         },
     }
     scanner := bufio.NewScanner(os.Stdin)
@@ -60,7 +155,7 @@ func main() {
         firstWord := words[0]
         cmd, ok := commands[firstWord]
         if ok {
-            err := cmd.callback()
+            err := cmd.callback(cfg)
             if err != nil {
                 fmt.Println(err)
             }
